470cd23306daa8b889843a98340c40486cb042e7
AadilBR <aadil.iphone@gmail.com>
2021-06-07 21:56:27 +0200

    test ico in progress

---
+++ contracts/ICO.sol
@@ -0,0 +1,101 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IToken.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+/**
+ * @title InitialCoinOffering ICO for Token "TT"
+ * @author Aadil
+ * @notice This contract is deployed with an ERC20 contract.
+ * Tokens and Ethers are blocked in the contract during 2 weeks.
+ * */
+contract ICO is Ownable {
+    using Address for address payable;
+
+    IToken private _token;
+    uint256 private _endOfIco;
+
+    constructor(address tokenAddress) {
+        _token = IToken(tokenAddress);
+        require(_token.balanceOf(_token.owner()) == 1000000 * 10**18, "ICO: The owner must have token TT to exchange");
+        _endOfIco = block.timestamp + 2 weeks;
+    }
+
+    event Bought(address indexed buyer, uint256 amount);
+    event Withdrew(address indexed owner_, uint256 amount);
+
+    /**
+     * @dev used to receive ether directly send from a transaction
+     * */
+    receive() external payable {
+        _buyToken(msg.sender, msg.value);
+    }
+
+    /**
+     * @dev used by the owner of the Token contract, the owner can withdraw
+     * all the ethers sent by the buyers in the ICO contract.
+     * But, he have to wait the end of the ICO period (2 weeks)
+     */
+
+    function withdraw() public onlyOwner {
+        require(
+            block.timestamp > _endOfIco,
+            "ICO : you have to wait 2 weeks from the deployement of this smartContract the ICO is still running."
+        );
+        uint256 gain = address(this).balance;
+        payable(msg.sender).sendValue(address(this).balance);
+        emit Withdrew(msg.sender, gain);
+    }
+
+    /**
+     * @dev used to buy erc20 Token "TT" using ether
+     */
+    function buyTokens() public payable {
+        _buyToken(msg.sender, msg.value);
+    }
+
+    /**
+     * @return Total Ether value in this ICO contract
+     */
+    function ICOBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    /**
+     * @return number of token "TT" sold by this ICO
+     */
+    function tokenSold() public view returns (uint256) {
+        return ConvertGweiToToken(ICOBalance()) / 10**18;
+    }
+
+    /**
+     * @dev Exchange rate 1 gwei = 1 token "TT"
+     * @param amount in Ether
+     * @return value in "TT"
+     */
+    function ConvertGweiToToken(uint256 amount) public pure returns (uint256) {
+        return amount * 10**9;
+    }
+
+    /**
+     * @dev this function allows the ICO contract to give in exchange of ethers TokenToken TT to buyers
+     * If the last buyer send more ethers than this contract can sell, he got the difference refund.
+     * @param sender the buyer
+     * @param amount in ether
+     */
+    function _buyToken(address sender, uint256 amount) private {
+        require(block.timestamp < _endOfIco, "ICO: Sorry ! The ICO is over, you can no longer buy token");
+        uint256 allowance = _token.allowance(_token.owner(), address(this));
+        require(allowance > 0, "ICO: has not been approved yet or all token are already sold");
+        uint256 token = ConvertGweiToToken(amount);
+        if (token > allowance) {
+            uint256 rest = token - allowance;
+            token -= rest;
+            payable(sender).sendValue(rest / 10**9);
+        }
+        _token.transferFrom(_token.owner(), sender, token);
+        emit Bought(sender, amount);
+    }
+}
---
+++ test/ICO-test.js
@@ -0,0 +1,50 @@
+const { expect } = require('chai');
+
+describe('ICO', function () {
+  let dev, owner, Token, token, ICO, ico, Alice, Bob, tx;
+  const INIT_SUPPLY = ethers.utils.parseEther('1000000');
+  const gwei = 10 ** 9;
+
+  beforeEach(async function () {
+    [dev, owner, Token, token, ICO, ico, Alice, Bob] = await ethers.getSigners();
+    // ERC20 deployment
+    Token = await ethers.getContractFactory('Token');
+    token = await Token.connect(dev).deploy(owner.address, 1000000);
+    await token.deployed();
+    // ICO deployment
+    ICO = await ethers.getContractFactory('ICO');
+    ico = await ICO.connect(dev).deploy(token.address);
+    await ico.deployed();
+    await token.connect(owner).approve(ico.address, INIT_SUPPLY);
+  });
+
+  describe('Deployment', function () {});
+
+  describe('Withdraw function', function () {
+    it('Should revert if not owner', async function () {
+      await expect(ico.connect(Bob).withdraw()).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+  });
+
+  describe('Buy function', function () {
+    it('receive', async function () {
+      tx = await Alice.sendTransaction({ to: ico.address, value: 2 * gwei });
+      expect(await token.balanceOf(Alice.address)).to.equal(ethers.utils.parseEther('2'));
+      expect(await ico.ICOBalance()).to.equal(2 * gwei);
+      expect(tx).to.changeEtherBalance(Alice, -2 * gwei);
+    });
+    it('buyToken', async function () {
+      tx = await ico.connect(Alice).buyTokens({ value: gwei });
+      expect(await token.balanceOf(Alice.address)).to.equal(ethers.utils.parseEther('1'));
+      expect(await ico.ICOBalance()).to.equal(gwei);
+      expect(tx).to.changeEtherBalance(Alice, -gwei);
+    });
+    it('Should emit a Bought event', async function () {
+      expect(await ico.connect(Alice).buyTokens({ value: gwei }))
+        .to.emit(ico, 'Bought')
+        .withArgs(Alice.address, gwei);
+    });
+  });
+
+  describe('Getter', function () {});
+});
